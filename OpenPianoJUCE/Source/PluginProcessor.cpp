90d09202809a89a9150a935dba14e19b8e94ef57
Michele Perrone <perrone.michele@outlook.com>
2022-02-02 17:29:19 +0100

    Add license notices in source files OpenPiano is licensed under the GNU Affero General Public License v3.0

--- OpenPianoJUCE/Source/PluginProcessor.cpp
+++ OpenPianoJUCE/Source/PluginProcessor.cpp
@@ -1,9 +1,20 @@
 /*
-  ==============================================================================
+OpenPiano: an open source piano engine based on physical modeling
+Copyright (C) 2021-2022 Michele Perrone
+Github: https://github.com/michele-perrone/OpenPiano
+Author e-mail: perrone(dot)michele(at)outlook(dot)com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This file contains the basic framework code for a JUCE plugin processor.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-  ==============================================================================
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "PluginProcessor.h"
